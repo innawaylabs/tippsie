41e60ae56cbdc94c52bb5deee358f3b10bf8b293
Ravi Kumar Mandala <ravikumar.mandala@gmail.com>
2017-08-16 01:29:16 -0700

    Initial commit

---
+++ Tippsie/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Tippsie
+//
+//  Created by Ravi Mandala on 8/15/17.
+//  Copyright © 2017 Ravi Mandala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipPercentagesSegControl: UISegmentedControl!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onScreenTap(_ sender: AnyObject) {
+        print ("Hello")
+    }
+    
+    @IBAction func amountUpdated(_ sender: AnyObject) {
+        let tipPercentages = [0.18, 0.2, 0.25]
+        let billAmount = Double(billField.text!) ?? 0
+        let tip = billAmount * tipPercentages[tipPercentagesSegControl.selectedSegmentIndex]
+        let total = billAmount + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }    
+}
+
